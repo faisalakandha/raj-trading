0af279c4f3f928e96797d4c10e4efae3c9995bae
Ahsan Sium <ahsansium28@gmail.com>
2022-04-17 11:01:18 +0600

    Coding

---
+++ src/renderer/Views/OrderBox/TradeOrderBox.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Lorem,
+    Button
+} from '@chakra-ui/react';
+
+const TradeOrderBox = ({ isOpen, onOpen, onClose }) => {
+
+    //const finalRef = React.useRef();
+
+    console.log(isOpen, onOpen, onClose);
+
+    return (
+        <div>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button colorScheme='blue' mr={3} onClick={onClose}>
+                            Close
+                        </Button>
+                        <Button variant='ghost'>Secondary Action</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </div>
+    )
+}
+
+export default TradeOrderBox;
